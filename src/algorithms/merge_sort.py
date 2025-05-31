64b5216248878d9e9cfd49e2fbebe322e7430317
Raghav Bhardwaj <109452860+ragbhard@users.noreply.github.com>
2025-05-31 21:35:35 +0530

    Update merge_sort.py

--- src/algorithms/merge_sort.py
+++ src/algorithms/merge_sort.py
@@ -1,20 +1,29 @@
 
 def merge_sort(arr, tracker):
-    def merge(left, right):
-        sorted_list = []
-        while left and right:
-            if tracker.compare(left[0], right[0]):
-                sorted_list.append(left.pop(0))
-            else:
-                sorted_list.append(right.pop(0))
-        return sorted_list + left + right
-
-    def sort(arr):
-        if len(arr) <= 1:
-            return arr
+    if len(arr) > 1:
         mid = len(arr) // 2
-        left = sort(arr[:mid])
-        right = sort(arr[mid:])
-        return merge(left, right)
+        L = arr[:mid]
+        R = arr[mid:]
 
-    return sort(arr.copy())
+        merge_sort(L, tracker)
+        merge_sort(R, tracker)
+
+        i = j = k = 0
+        while i < len(L) and j < len(R):
+            if tracker.compare(L[i], R[j]):
+                arr[k] = L[i]
+                i += 1
+            else:
+                arr[k] = R[j]
+                j += 1
+            k += 1
+
+        while i < len(L):
+            arr[k] = L[i]
+            i += 1
+            k += 1
+
+        while j < len(R):
+            arr[k] = R[j]
+            j += 1
+            k += 1
